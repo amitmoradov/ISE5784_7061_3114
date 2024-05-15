ea93a000d122f63d60830667eb779975ef7048a2
Amit <moamit21221@gmail.com>
2024-05-15 13:45:27 +0300

    unit Tests

---
+++ unittest/primitives/Double3Test.java
@@ -0,0 +1,44 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Double3Test {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void reduce() {
+    }
+
+    @Test
+    void product() {
+    }
+
+    @Test
+    void lowerThan() {
+    }
+
+    @Test
+    void testLowerThan() {
+    }
+}
---
+++ unittest/primitives/VectorTest.java
@@ -0,0 +1,40 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void dotProduct() {
+    }
+
+    @Test
+    void crossProduct() {
+    }
+
+    @Test
+    void lengthSquared() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void normalize() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
