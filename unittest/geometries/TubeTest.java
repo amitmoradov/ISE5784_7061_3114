dbbf9c0a6b53051820c662a61aff2cbc2971c8fa
Yshaul2000 <yinon.shaul99@gmail.com>
2024-05-19 15:45:28 +0300

    Test in Tube .

--- unittest/geometries/TubeTest.java
+++ unittest/geometries/TubeTest.java
@@ -25,14 +25,11 @@ class TubeTest {
         assertEquals(new Vector(0,1,0), tube.getNormal(new Point(1,1,1)),"ERROR:" +
                 " getNormal() does not return the correct normal vector");
 
-
-
         // ================= Boundary Values Tests ==================
 
         // TC02: Testing when p - p0 is orthogonal to direction of the ray in the tube
-//        Tube tube2 = new Tube(new Ray(new Point(0, 0, 0), new Vector(0, 0, 1)), 1);
-//
-//        assertNotEquals(new Vector(1, 0, 0), tube2.getNormal(new Point(1, 0, 0)),
-//                "ERROR: the point P is in front of the head of ray");
+        Tube tube2 = new Tube(new Ray(new Point(0, 0, 0), new Vector(0, 0, 1)), 1);
+        assertEquals(new Vector(1, 0, 0), tube2.getNormal(new Point(1, 0, 0)),
+                "ERROR: the calculate when point P is in front of the head of ray in the tube not correct");
     }
 }
