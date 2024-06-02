5c2559aca141f276837087e098ab1c6d1e5c17b3
Yshaul2000 <yinon.shaul99@gmail.com>
2024-06-02 14:55:45 +0300

    findIntersections triangle .

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -10,19 +10,36 @@ import java.util.List;
  * Extends the Polygon class.
  */
 public class Triangle extends Polygon {
-        /**
-        * Constructs a triangle from three points.
-        *
-        * @param p1 The first point of the triangle.
-        * @param p2 The second point of the triangle.
-        * @param p3 The third point of the triangle.
-        */
-        public Triangle(Point p1, Point p2, Point p3) {
-            super(p1, p2, p3);
-        }
+    /**
+     * Constructs a triangle from three points.
+     *
+     * @param p1 The first point of the triangle.
+     * @param p2 The second point of the triangle.
+     * @param p3 The third point of the triangle.
+     */
+    public Triangle(Point p1, Point p2, Point p3) {
+        super(p1, p2, p3);
+    }
 
-        @Override
-        public List<Point> findIntersections(Ray ray) {
-            Vector v1 =
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+        List<Point> intersections = plane.findIntersections(ray);
+
+        Vector v1 = vertices.get(0).subtract(ray.getHead());
+        Vector v2 = vertices.get(1).subtract(ray.getHead());
+        Vector v3 = vertices.get(2).subtract(ray.getHead());
+
+        Vector n1 = v1.crossProduct(v2).normalize();
+        Vector n2 = v2.crossProduct(v3).normalize();
+        Vector n3 = v3.crossProduct(v1).normalize();
+
+        double vn1 = ray.getDirection().dotProduct(n1);
+        double vn2 = ray.getDirection().dotProduct(n2);
+        double vn3 = ray.getDirection().dotProduct(n3);
+
+        if (vn1 > 0 && vn2 > 0 && vn3 > 0 || vn1 < 0 && vn2 < 0 && vn3 < 0) {
+            return intersections;
         }
+        return null;
+    }
 }
