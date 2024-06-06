d35c0b038ab11bf7362dfe5c311a22632ae251aa
Yshaul2000 <yinon.shaul99@gmail.com>
2024-06-06 21:28:53 +0300

    Camera class .

---
+++ src/renderer/Camera.java
@@ -0,0 +1,77 @@
+package renderer;
+
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+/**
+ * Represents a camera in three-dimensional space.
+ * The camera is used to render images of the scene.
+ */
+
+
+public class Camera implements Cloneable {
+
+    private Point p0; // The camera's location
+    private Vector vTo; // The vector pointing towards the view plane
+    private Vector vUp; // The vector pointing upwards
+    private Vector vRight; // The vector pointing to the right
+
+    double width = 0.0; // The width of the view plane
+    double height = 0.0; // The height of the view plane
+    double distance = 0.0; // The distance between the camera and the view plane
+
+    Point getP0() {
+        return p0;
+    }
+
+    Vector getVTo() {
+        return vTo;
+    }
+
+    Vector getVUp() {
+        return vUp;
+    }
+
+    Vector getVRight() {
+        return vRight;
+    }
+
+    double getWidth() {
+        return width;
+    }
+
+    double getHeight() {
+        return height;
+    }
+
+    double getDistance() {
+        return distance;
+    }
+
+    /**
+     * A builder class for constructing a Camera object.
+     */
+    private Camera(){
+    }
+
+    /**
+     * Returns a new object of A Builder class
+     */
+    public getBuilder(){
+        return new Builder();
+    }
+
+    /**
+     * Creating a ray through the center of the pixel
+     * @param nx The number of pixels in the x direction
+     * @param ny The number of pixels in the y direction
+     * @param j  The x index of the pixel
+     * @param i  The y index of the pixel
+     * @return
+     */
+    public Ray construcRay(int nx, int ny, int j, int i){
+         return null;
+    }
+}
+
