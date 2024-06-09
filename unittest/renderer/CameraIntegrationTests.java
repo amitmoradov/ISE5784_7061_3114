81f334b8630fc9f9aa28575102d929e32ed5bfa3
Amit <moamit21221@gmail.com>
2024-06-09 13:11:16 +0300

    test of intrection in camera

--- unittest/renderer/CameraIntegrationTests.java
+++ unittest/renderer/CameraIntegrationTests.java
@@ -1,5 +1,25 @@
 package renderer;
 
+import geometries.Intersectable;
+import primitives.Ray;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CameraIntegrationTests {
+
+    private void testRayIntersections(Camera camera, Intersectable geometry, int expectedIntersections,
+                                      int actualIntersections) {
+        int count = 0;
+        int nX = 3, nY = 3;
+
+        for (int i = 0; i < nY; i++) {
+            for (int j = 0; j < nX; j++) {
+                Ray ray = camera.constructRay(nX, nY, j, i);
+                count += geometry.findIntersections(ray).size();
+            }
+        }
+
+        assertEquals(expectedIntersections, count, "Wrong number of intersections");
+    }
 
 }
