30edda4644f7bf036caa4a0dcec2b92ec31238fb
Yshaul2000 <yinon.shaul99@gmail.com>
2024-07-10 21:42:41 +0300

    Marks .

--- unittest/renderer/ALLItemsTest.java
+++ unittest/renderer/ALLItemsTest.java
@@ -20,8 +20,8 @@ public class ALLItemsTest {
     public void allItems() {
         Scene scene = new Scene("Test scene");
         Camera.Builder cameraBuilder = Camera.getBuilder()
-                .setLocation(new Point(0, -100, 0))
-                .setVpDistance(100)
+                .setLocation(new Point(0, -300, 0))
+                .setVpDistance(300)
                 .setDirection(new Vector(0, 1, 0), new Vector(0, 0, 1))
                 .setRayTracer(new SimpleRayTracer(scene));
 
@@ -35,8 +35,8 @@ public class ALLItemsTest {
                 new Sphere(new Point(0, 100, 5), 50).setEmission(new Color(70,0,10))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100)),
                 // Sphere to show right eye
-                new Sphere(new Point(25, 100,0), 50.setEmission(new Color(255, 255, 255))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100)),
+                new Sphere(new Point(120,100,0),50).setEmission(new Color(BLACK))
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30)),
                 // Sphere to show left eye
                 new Sphere(new Point(-25, -25, 15), 0d).setEmission(new Color(255, 255, 255))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100))
@@ -47,7 +47,7 @@ public class ALLItemsTest {
 //
         scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.15)).setBackground(new Color(75, 120, 130));
 
-        cameraBuilder.setVpSize(200, 200)
+        cameraBuilder.setVpSize(300, 300)
                 .setImageWriter(new ImageWriter("allItemsP", 600, 600))
                 .build()
                 .renderImage()
