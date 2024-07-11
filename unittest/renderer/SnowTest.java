740b2b8861cf6a137fb22e352b4af1638b025852
Yshaul2000 <yinon.shaul99@gmail.com>
2024-07-11 13:33:39 +0300

    Pic .

--- unittest/renderer/SnowTest.java
+++ unittest/renderer/SnowTest.java
@@ -1,19 +1,27 @@
 package renderer;
 
-import static java.awt.Color.*;
-
-import geometries.*;
-import org.junit.jupiter.api.Test;
-
+import geometries.Plane;
+import geometries.Sphere;
 import lighting.AmbientLight;
+import lighting.DirectionalLight;
+import lighting.PointLight;
 import lighting.SpotLight;
-import primitives.*;
+import org.junit.jupiter.api.Test;
+import primitives.Color;
+import primitives.Material;
+import primitives.Point;
+import primitives.Vector;
+import geometries.Polygon;
 import scene.Scene;
 
-public class SnowmanTest {
+import static java.awt.Color.*;
 
-    /** A scene with ten different bodies that combines the material of the bodies, shining light, shadow, mirroring,
-     *  and reflection */
+
+public class SnowTest {
+    /**
+     * A scene with ten different bodies that combines the material of the bodies, shining light, shadow, mirroring,
+     * and reflection
+     */
     @Test
     public void Snow() {
         Scene scene = new Scene("Test scene");
@@ -21,19 +29,19 @@ public class SnowmanTest {
                 .setLocation(new Point(0, -200, 100))
                 .setVpDistance(200)
 
-                .setDirection(new Vector(0, 1,0), new Vector( 0, 0,1))
+                .setDirection(new Vector(0, 1, 0), new Vector(0, 0, 1))
                 .setRayTracer(new SimpleRayTracer(scene));
 
 
         scene.geometries.add(
 
-                new Plane(new Point(0,0, -50), new Vector(0, 0, 1)).setEmission(new Color(200,125,50))
+                new Plane(new Point(0, 0, -50), new Vector(0, 0, 1)).setEmission(new Color(200, 125, 50))
                         .setMaterial(new Material().setKd(0.5).setShininess(100)),
 
                 new Sphere(new Point(0, 100, 5), 50).setEmission(new Color(180, 180, 255))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100)),
 
-                new Sphere(new Point(0,100,70),35).setEmission(new Color(180, 180, 255))
+                new Sphere(new Point(0, 100, 70), 35).setEmission(new Color(180, 180, 255))
                         .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30)),
 
 
@@ -41,7 +49,7 @@ public class SnowmanTest {
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)),
 
                 // Cipa
-                new Sphere(new Point(0, 100, 126), 15).setEmission(new Color(116,  138,250))
+                new Sphere(new Point(0, 100, 126), 15).setEmission(new Color(116, 138, 250))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)),
 
                 // Sphere to show left eye
@@ -59,7 +67,7 @@ public class SnowmanTest {
                 new Sphere(new Point(0, 30, -20), 26).setEmission(new Color((BLACK)))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setKR(0.6).setShininess(100)),
 
-                new Sphere(new Point(86, 50, -10), 26).setEmission(new Color(0,78,188))
+                new Sphere(new Point(86, 50, -10), 26).setEmission(new Color(0, 78, 188))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setKT(0.4).setShininess(100)),
 
                 new Sphere(new Point(-82, 40, -16), 26).setEmission(new Color((GREEN)))
@@ -81,25 +89,26 @@ public class SnowmanTest {
                         new Point(-180, 250, 150),
                         new Point(-180, 0, 150),
                         new Point(-180, 0, -50)
-                ).setEmission(new Color(70, 130, 180))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100).setKT(0.7)),
+                ).setEmission(new Color(BLACK))
+                        .setMaterial(new Material().setKd(0.1).setKs(0.6).setKR(1.0).setShininess(100)),
 
-                // Polygon to create a higher wall behind the snowman
+                // Polygon to create a back wall that is straight and vertical
                 new Polygon(
-                        new Point(-180, 300, 150),
-                        new Point(180, 300, 150),
-                        new Point(180, 0, 150),
-                        new Point(-180, 0, 150)
-                ).setEmission(new Color(70, 130, 180))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100).setKT(0.7))
+                        new Point(-170, 260, 150),
+                        new Point(170, 260, 150),
+                        new Point(170, 260, -50),
+                        new Point(-170, 260, -50)
+                ).setEmission(new Color(BLACK))
+                        .setMaterial(new Material().setKd(0.1).setKs(0.8).setKR(1.0).setShininess(100))
         );
         // Add a light sources
-        scene.lights.add(new SpotLight(new Color(1000,600,600), new Point(-500, 250, 500),
+        scene.lights.add(new SpotLight(new Color(1000, 600, 600), new Point(-500, 250, 500),
                 new Vector(1, -1, 0)).setKl(0.0004).setKq(0.0000006));
 
-        // Add a direction light
-        scene.lights.add(new SpotLight(new Color(YELLOW), new Point(-80, 200, 150),
-                new Vector(0, 1, 0)).setKl(0.0004).setKq(0.0000006));
+
+        // Add a point light with blue color
+        scene.lights.add(new PointLight(new Color(0, 0, 255), new Point(0, 92, 148))
+                .setKl(0.0004).setKq(0.0000006));
 
 //
         scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.15)).setBackground(new Color(75, 120, 130));
