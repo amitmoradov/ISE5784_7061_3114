da21d65ec603f27c14571a60aa60fe5f6f2cdd73
Amit <moamit21221@gmail.com>
2024-07-16 18:28:09 +0300

    create MP1test2

--- unittest/renderer/MP1test2.java
+++ unittest/renderer/MP1test2.java
@@ -22,13 +22,13 @@ public class MP1test2 {
         scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
 
         scene.geometries.add(
-                new Sphere(new Point(-80, 10, -50), 50)
+                new Sphere(new Point(-50, 40, 40), 50)
                         .setMaterial(new Material().setKd(0.3).setKs(0.5).setShininess(10).setKR(0.3)// .setKt(0.5 5)
                                 .setkG(10)),
                 new Sphere(new Point(60, 0, 20), 35d)
                         .setMaterial(new Material().setKd(0.6).setKs(0.2).setShininess(3).setKR(0.4).setKT(0.2))
                         .setEmission(new Color(252, 148, 3)),
-                new Sphere(new Point(-15, -40, 10), 25d)
+                new Sphere(new Point(-40, -40, 10), 25d)
                         .setMaterial(new Material().setKd(0.3).setKs(0.7).setShininess(5).setKR(0.7).setKT(0.2))
                         .setEmission(new Color(252, 3, 252)),
 
@@ -49,7 +49,7 @@ public class MP1test2 {
                 new Triangle(new Point(-160, -160, -115), new Point(-70, -70, -140), new Point(75, -75, -150)) //
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)));//
 
-        scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(60, 50, 0), new Vector(0, 0, -1)) //
+        scene.lights.add(new SpotLight(new Color(200, 400, 200), new Point(60, 50, 0), new Vector(0, 0, -1)) //
                 .setKl(4E-5).setKq(2E-7));
 
         cameraBuilder //
